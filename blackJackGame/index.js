484a6943a539095a15071ced9873607efbb2a954
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-07 00:41:56 +0100

    added an id to my cards paragraph and used it to manipulate the content

--- blackJackGame/index.js
+++ blackJackGame/index.js
@@ -6,6 +6,7 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
   if (sum <= 20) {
@@ -19,6 +20,7 @@ function startGame() {
   }
 
   // sumEl.innerText = `"Sum: ${sum}"`;
+  cardsEl.innerText = "Cards: " + firstCard + " " + secondCard;
   sumEl.innerText = "Sum: " + sum;
   messageEl.innerText = message;
 
