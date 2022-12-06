0e55f5f1b366981e588f2f4bae08437a10aa8774
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-07 00:12:04 +0100

    style the Blackjack game and created the function in the index.js

---
+++ blackJackGame/index.js
@@ -0,0 +1,27 @@
+let firstCard = 6;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.innerText = message;
+
+  //CASH OUT
+  console.log(hasBlackJack);
+
+  //GAME OVER
+  console.log(isAlive);
+}
