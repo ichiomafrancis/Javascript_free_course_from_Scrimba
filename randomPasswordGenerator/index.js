0c8af737a3e53ed7bf8e04384b9d50f776f195c2
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-16 15:58:28 +0100

    I added comments to the index.js to make it readable to anyone

--- randomPasswordGenerator/index.js
+++ randomPasswordGenerator/index.js
@@ -1,3 +1,4 @@
+/* Separated the characters into two parts: The first array contains all the characters including the special characters while the second array does not contain the special characters.*/
 const withSpecialCharacters = [
   "A",
   "B",
@@ -157,15 +158,23 @@ const withoutSpecialCharacters = [
   "9",
 ];
 
+// Get the element for the password display fields using its id
 let passwordOneEl = document.getElementById("passwordOne-el");
 let passwordTwoEl = document.getElementById("passwordTwo-el");
 
+//Function to generate random passwords according to the users given conditions
 function generateRandomPassword() {
+  //Declared the password variables
   let passwordOne = "";
   let passwordTwo = "";
+  //Declared the variable to store the value of the checkbox
   let checkboxEl = document.getElementById("checkbox-el").checked;
+  //Read the value of the input field where the user indicates their desired length for the generated password
   let passwordLengthEl = document.getElementById("passwordLength-el").value;
+  /*if true, this indicates that the user wants to include special characters in the random generated password.
+  So we use the first array that contains all the characters including the special characters*/
   if (checkboxEl === true) {
+    //loop through the characters according to the specified password length by the user to generate random passwords
     for (let i = 0; i < passwordLengthEl; i++) {
       passwordOne +=
         withSpecialCharacters[
@@ -177,6 +186,8 @@ function generateRandomPassword() {
         ];
     }
   } else {
+    /*loop through the characters according to the specified password length by the user.
+    So we use the second array that does not contain the special characters*/
     for (let i = 0; i < passwordLengthEl; i++) {
       passwordOne +=
         withoutSpecialCharacters[
@@ -188,6 +199,7 @@ function generateRandomPassword() {
         ];
     }
   }
+  //Set the content of the password field to the newly generated random passwords
   passwordOneEl.textContent = passwordOne;
   passwordTwoEl.textContent = passwordTwo;
 }
