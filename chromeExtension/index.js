f237eaab35f7e53e0bc18b0c324605acd88d5478
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-17 21:43:59 +0100

    my first commit for the chrome extension project

---
+++ chromeExtension/index.js
@@ -0,0 +1,18 @@
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+// Get the button element and add a click event listener to it
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+  //OR
+  /*const li = document.createElement("li")
+  li.textContent = myLeads[i]
+  ulEl.append(li)*/
+}
