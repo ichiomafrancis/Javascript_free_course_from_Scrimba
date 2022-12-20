6ae121fc52789bed75d3cdba9778c20c920475e1
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-20 21:09:09 +0100

    created a renderLead function to make my code reusable and also added some code to clear the screen once the value has been read

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 // Get the button element and add a click event listener to it
 const inputBtn = document.getElementById("input-btn");
@@ -8,15 +8,20 @@ const ulEl = document.getElementById("ul-el");
 //Add event listener to the button and also perform the function once the button is clicked
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log(myLeads);
+  innerEl.value = "";
+  renderLeads();
 });
 
-//Loop through the array
-for (let i = 0; i < myLeads.length; i++) {
-  // Add the results of the loop to the ul element
-  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-  //Another method to add the results of the loop to the ul element
-  /*const li = document.createElement("li")
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
+// ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+//Another method to add the results of the loop to the ul element
+/*const li = document.createElement("li")
   li.textContent = myLeads[i]
   ulEl.append(li)*/
-}
