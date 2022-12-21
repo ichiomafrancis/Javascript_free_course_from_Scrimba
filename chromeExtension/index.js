cd2d54388a1fb795b4f2c1d6a4e67e5f13fa2ad4
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-22 00:07:09 +0100

    made my code reusable / more dynamic by using parameter in my render function. This enables the function to accept any arguments passed into it

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -12,32 +12,16 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-//Add event listener to the button and also perform the function once the button is clicked
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  //JSON.stringify converts the myLeads array to a string.
-  //localStorage.setItem("key", "value") - Stores key value pairs in the local storage.
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a href='${myLeads[i]}' target="_blank">
-        ${myLeads[i]}
+      <a href='${leads[i]}' target="_blank">
+        ${leads[i]}
       </a>
     </li>`;
   }
@@ -49,3 +33,19 @@ function renderLeads() {
 /*const li = document.createElement("li")
   li.textContent = myLeads[i]
   ulEl.append(li)*/
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+//Add event listener to the button and also perform the function once the button is clicked
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  //JSON.stringify converts the myLeads array to a string.
+  //localStorage.setItem("key", "value") - Stores key value pairs in the local storage.
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
