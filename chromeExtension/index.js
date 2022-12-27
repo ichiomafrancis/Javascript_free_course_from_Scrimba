e13eaf77ba65643cf6969175311c707f10188459
Francis Chioma <ichiomafrancis@gmail.com>
2022-12-27 10:36:51 +0100

    used the save btn to add a url to the myLeads array and then save it in local storage and render it

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -19,7 +19,10 @@ if (leadsFromLocalStorage) {
 const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
 
 tabBtn.addEventListener("click", function () {
-  console.log(tabs[0].url);
+  // console.log(tabs[0].url);
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
